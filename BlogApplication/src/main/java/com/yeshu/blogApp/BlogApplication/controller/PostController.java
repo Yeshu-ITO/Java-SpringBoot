56dab232c971a2e7e5c0722c2f81e34c7f12eff8
Yeshu <yeshu@gmail.com>
2024-06-13 12:54:59 +0530

    Blog Application added image API

--- BlogApplication/src/main/java/com/yeshu/blogApp/BlogApplication/controller/PostController.java
+++ BlogApplication/src/main/java/com/yeshu/blogApp/BlogApplication/controller/PostController.java
@@ -4,12 +4,20 @@ import com.yeshu.blogApp.BlogApplication.config.AppConstants;
 import com.yeshu.blogApp.BlogApplication.payloads.ApiResponse;
 import com.yeshu.blogApp.BlogApplication.payloads.PostDto;
 import com.yeshu.blogApp.BlogApplication.payloads.PostResponse;
+import com.yeshu.blogApp.BlogApplication.service.FileService;
 import com.yeshu.blogApp.BlogApplication.service.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -18,6 +26,12 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable Integer userId,@PathVariable Integer categoryId){
@@ -76,4 +90,23 @@ public class PostController {
         List<PostDto> posts = this.postService.searchPostByContent(keywords);
         return new ResponseEntity<List<PostDto>>(posts,HttpStatus.OK);
     }
+
+    @PostMapping("/posts/image/upload/{postId}")
+    public ResponseEntity<PostDto> uploadPostImage(@RequestParam("image")MultipartFile image, @PathVariable Integer postId) throws IOException {
+
+        PostDto postdto = this.postService.getPostById(postId);
+
+        String  fileName = this.fileService.uploadImage(path,image);
+        postdto.setImageName(fileName);
+        PostDto updatePost = this.postService.updatePost(postdto,postId);
+        return  new ResponseEntity<>(updatePost,HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/posts/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(@PathVariable String imageName, HttpServletResponse response) throws IOException{
+        InputStream resource = this.fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource,response.getOutputStream());
+    }
+
 }
